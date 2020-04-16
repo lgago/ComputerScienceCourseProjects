ca7a92274bfc9d812c52dd865a9c71c8ca88c940
Luis Gago <luisg@qualtrics.com>
2020-04-16 10:02:53 -0600

    final class project

---
+++ Project7 - Concurrent Programming/cia_a.py
@@ -0,0 +1,32 @@
+from time import perf_counter
+from math import sqrt
+import requests
+
+
+#Wrap this in download func and pass in flag country code for proj 7
+def download(countryCode):# Download a flag from the CIA
+    prefix = 'https://www.cia.gov/library/publications/resources/the-world-factbook/graphics/flags/large/'
+    fname = prefix + countryCode + "-lgflag.gif"
+    flag = requests.get(fname).content      # Note: No headers={...}
+    if flag:
+        with open(countryCode + ".gif","wb") as f:
+            f.write(flag)
+        return len(flag)
+
+def main():
+    with open("flags.txt","r") as f:
+        countryCodes = f.read().split('\n')
+
+    numBytes = 0
+
+    start = perf_counter()
+    numBytes = sum(map(download, countryCodes))
+    stop = perf_counter()
+
+    #These numbers need to be output to a file. 
+    file1 = open("cia_a.txt","w")
+    file1.write("Elapsed time: " + str(stop - start) + "\n" + str(numBytes) + " bytes downloaded") 
+    file1.close() 
+
+if __name__ == "__main__":
+    main()
